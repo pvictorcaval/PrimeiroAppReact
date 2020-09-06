e2eba069008da70bb61389206524ce41305b1227
p.cavalcante.costa <p.cavalcante.costa@avanade.com>
2020-09-06 04:12:46 -0300

    Initial commit

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Container, Header, Content, Form, Item, Input, Left, Body, Right, Title, Subtitle } from 'native-base';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+        <Header>
+          <Left/>
+          <Body>
+            <Title>Apto</Title>
+            <Subtitle>Seja bem vindo!</Subtitle>
+          </Body>
+          <Right />
+        </Header>
+        <Content>
+          <Form>
+            <Item>
+              <Input placeholder="Email ou Usuario" />
+            </Item>
+            <Item last>
+              <Input placeholder="Senha" />
+            </Item>
+          </Form>
+        </Content>
+      </Container>
   );
 }
 
